b1ddede3f9705087f4464e23b49340cbc364cb9a
Eugene Ivanov <most1360@yandex.ru>
2022-06-08 16:51:08 +0300

    Done

--- app/src/main/java/com/example/coroutinesapp/MainActivity.kt
+++ app/src/main/java/com/example/coroutinesapp/MainActivity.kt
@@ -5,6 +5,16 @@ import android.os.Bundle
 import androidx.fragment.app.FragmentManager
 
 class MainActivity : AppCompatActivity() {
+    /*
+    b) Корутина - это легковесная альтернатива потока.
+    Плюсы
+    - Дешевые и менее затратные в сравнении с потоками
+    - Знают о ЖЦ компонентов
+    - в сравнении с потоками, не блокируются, а приостанавливают свое выполнение.
+    Минус
+    - отладка
+     */
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
